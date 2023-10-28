c2d627d20601e395a89b273eb1f2b48f8c7a8e39
nitesh sinwar <w3docs_email@example.com>
2023-10-28 10:17:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors')
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const secretKey = 'your_secret_key'; // Replace with your secret key.
+
+// Route for user authentication and token generation.
+app.post('/authenticate', (req, res) => {
+  // Authenticate the user (e.g., username and password).
+  const username = req.body.username;
+  console.log('username',username);
+  const password = req.body.password;
+  console.log('password',password);
+  // If authentication is successful, generate an access token.
+  if (username === 'valid_user' && password === 'password') {
+    const accessToken = jwt.sign({ username }, secretKey);
+    console.log('accessToken',accessToken);
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+});
+
+// Secure route to receive the Account data from Salesforce.
+app.post('/salesforce/account', (req, res) => {
+  const salesforceToken = req.header('Authorization');
+  console.log('salesforceToken',salesforceToken);
+  jwt.verify(salesforceToken, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // Extract and log the Account data received from Salesforce.
+    const accountData = req.body;
+    console.log('Received Account Data:');
+    console.log(accountData);
+
+    // Perform any additional processing you need.
+    // ...
+
+    res.status(200).json({ message: 'Account data received successfully' });
+  });
+});
+
+const port = 3000; // Set the port you want to use.
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
