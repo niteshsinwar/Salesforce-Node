b3b7817b063fc66eb4d495619623d6939d7d5dda
nitesh sinwar <w3docs_email@example.com>
2023-10-28 10:39:23 +0530

    final commit

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const secretKey = 'your_secret_key'; // Replace with your secret key.
-const accessToken='0';
-const salesforceToken ='1';
+
 // Route for user authentication and token generation.
 app.post('/authenticate', (req, res) => {
   // Authenticate the user (e.g., username and password).
@@ -19,7 +18,7 @@ app.post('/authenticate', (req, res) => {
   console.log('password',password);
   // If authentication is successful, generate an access token.
   if (username === 'valid_user' && password === 'password') {
-     accessToken = jwt.sign({ username }, secretKey);
+    const accessToken = jwt.sign({ username }, secretKey);
     console.log('accessToken',accessToken);
     res.status(200).json({ accessToken });
   } else {
@@ -29,31 +28,24 @@ app.post('/authenticate', (req, res) => {
 
 // Secure route to receive the Account data from Salesforce.
 app.post('/salesforce/account', (req, res) => {
-  salesforceToken = req.header('Authorization');
+  const salesforceToken = req.header('Authorization');
   console.log('salesforceToken',salesforceToken);
-
-  /*jwt.verify(salesforceToken, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    const accountData = req.body;
-    console.log('Received Account Data:');
-    console.log(accountData);
-
-
-    res.status(200).json({ message: 'Account data received successfully' });
-  });*/
   
+  jwt.verify(salesforceToken, secretKey, (err, decoded) => {
+    if (err) {
+      console.log(err);
+    }
 
-    if (salesforceToken==accessToken) {
- 
+    // Extract and log the Account data received from Salesforce.
     const accountData = req.body;
     console.log('Received Account Data:');
     console.log(accountData);
 
-    res.status(200).json({ message: 'Account data received successfully' });
-  };
+    // Perform any additional processing you need.
+    // ...
 
+    res.status(200).json({ message: 'Account data received successfully' });
+  });
 });
 
 const port = 3000; // Set the port you want to use.
